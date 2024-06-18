3f415d335ecb960266952956c2f63c948c89e1ee
Aaron Feledy <aaron@arrowonesolutions.com>
2024-06-18 15:31:50 -0400

    Upgrade gradle plugin to 2.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
-rootProject.name = "lando-intellij-plugin"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "Lando Local Development Environment"
